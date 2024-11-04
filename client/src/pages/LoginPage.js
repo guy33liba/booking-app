19dfae4c9a4e8ef5cd6857b4591263926019933d
Guy Liba <guy33liba@gmail.com>
2024-11-04 17:02:26 +0200

    ds

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <div className="mt-4">
       <h1 className="text-4xl text-center">Login</h1>
-      <form className="max-w-2x; mx-auto border">
+      <form className="max-w-md mx-auto border">
         <input type="email" placeholder="'your@email.com" />
         <input type="password" placeholder="password" />
         <button>Login</button>
