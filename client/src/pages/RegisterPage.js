8f168a25734660d2e62e937f1241b1f894d3b47a
Guy Liba <guy33liba@gmail.com>
2024-11-04 18:26:24 +0200

    rege

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -5,11 +5,12 @@ const RegisterPage = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  function registerUser() {}
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto ">
+        <form className="max-w-md mx-auto " onSubmit={registerUser}>
           <input
             type="text"
             placeholder="John Doe"
