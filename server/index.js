39893b7a9900b8ec1aacdb2b387a099c4c8e249c
Guy Liba <guy33liba@gmail.com>
2024-11-04 18:41:15 +0200

    ass

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express"
 import cors from "cors"
 import User from "./models/User.js"
+
 const app = express()
 
-app.use(cors({ credentials: "true", origin: "http://localhost:3000" }))
 app.use(express.json())
+app.use(cors({ credentials: "true", origin: "http://localhost:3000" }))
 
 app.post("/register", (req, res) => {
   const { name, password, email } = req.body
