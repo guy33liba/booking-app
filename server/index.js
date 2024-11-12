f2784315d9858d64caf9a783f66ee355e988d72c
Guy Liba <guy33liba@gmail.com>
2024-11-12 16:04:50 +0200

    login

--- server/index.js
+++ server/index.js
@@ -32,10 +32,11 @@ app.post("/register", async (req, res) => {
 })
 app.post("/login", async (req, res) => {
   const { email, password } = req.body
-  const user = await User.findOne({ email })
-  if (!user) return res.status(404).json({ message: "User not found" })
-  const isMatch = bcrypt.compareSync(password, user.password)
-  if (!isMatch) return res.status(401).json({ message: "Invalid password" })
-  res.json({ message: "Login successful" })
+  const userDoc = await User.findOne({ email })
+  if (userDoc) {
+    res.json("found")
+  } else {
+    res.json("not found")
+  }
 })
 app.listen(4000, console.log("hello"))
