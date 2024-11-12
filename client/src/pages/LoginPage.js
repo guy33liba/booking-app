0e670489ed948be06cbbea3c5bbf9ec3f710b9a5
Guy Liba <guy33liba@gmail.com>
2024-11-12 16:03:22 +0200

    login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
-
+import axios from "axios"
 const LoginPage = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.post("http://localhost:4000/login", { email, password })
+      alert("Login successful!")
+    } catch (error) {
+      alert("Login failed!")
+    }
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto ">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto " onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
             Don't have an account Yet?{" "}
--- server/index.js
+++ server/index.js
@@ -27,7 +27,15 @@ app.post("/register", async (req, res) => {
     })
     res.json(userDoc)
   } catch (error) {
-    res.status(422).json(error.message)
+    res.status(422).json(error)
   }
+})
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (!user) return res.status(404).json({ message: "User not found" })
+  const isMatch = bcrypt.compareSync(password, user.password)
+  if (!isMatch) return res.status(401).json({ message: "Invalid password" })
+  res.json({ message: "Login successful" })
 })
 app.listen(4000, console.log("hello"))
